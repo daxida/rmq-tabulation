9e0f860bfa81da957b37d5500fc6b0617dd5ff94
daxida <galmaw@hotmail.com>
2024-02-29 07:28:28 +0100

    exporting rmq to a single file

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,8 @@
+mod rmq;
+
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
-
-#[inline(always)]
-fn flog2(v: usize) -> usize {
-    v.ilog2() as usize
-}
-
-trait Rmq {
-    fn rmq(&self, i: usize, j: usize) -> usize;
-}
-
-struct Sparse {
-    lgn: usize,
-    table: Vec<usize>, // convert this to a slice
-}
-
-impl Sparse {
-    fn new(t: &[usize]) -> Self {
-        let n = t.len();
-        let lgn = flog2(n);
-
-        let mut table: Vec<usize> = vec![0; (lgn + 1) * n];
-
-        for j in 0..n {
-            table[j * (lgn + 1)] = t[j];
-        }
-
-        // Build the sparse table
-        for i in 1..=lgn {
-            for j in 0..=(n - (1 << i)) {
-                table[j * (lgn + 1) + i] = std::cmp::min(
-                    table[j * (lgn + 1) + i - 1],
-                    table[(j + (1 << (i - 1))) * (lgn + 1) + i - 1],
-                );
-            }
-        }
-
-        Sparse { lgn, table }
-    }
-}
-
-impl Rmq for Sparse {
-    fn rmq(&self, i: usize, j: usize) -> usize {
-        debug_assert!(i < j);
-
-        let k = flog2(j - i + 1);
-        std::cmp::min(
-            self.table[i * (self.lgn + 1) + k],
-            self.table[(j + 1 - (1 << k)) * (self.lgn + 1) + k],
-        )
-    }
-}
+use rmq::{Rmq, Sparse};
 
 fn create_random_ary(ary_size: usize, max_ary: usize) -> Vec<usize> {
     let mut rng = StdRng::from_entropy();
