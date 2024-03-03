4f4bb6219bd2d04f3a885f65b3f132e122d7e1a7
daxida <galmaw@hotmail.com>
2024-03-03 13:00:39 +0100

    Remove Point and Power classes

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@ fn flog2(v: usize) -> usize {
     v.ilog2() as usize
 }
 
+// Try this too
+pub fn log2_down(j: usize) -> usize {
+    assert!(j != 0);
+    (usize::BITS - j.leading_zeros() - 1) as usize
+}
+
 pub trait RMQ {
     fn rmq(&self, i: usize, j: usize) -> Option<usize>;
 }
@@ -51,31 +57,6 @@ impl RMQ for Sparse {
 
 mod matrix;
 use matrix::Matrix;
-
-/// An index i together with its value x[i].
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Point(usize, usize);
-impl Point {
-    pub fn new(i: usize, x: &[usize]) -> Point {
-        Point(i, x[i])
-    }
-    /// Get Some(Point(i,x[i])) if the index is valid or None if not
-    pub fn get(i: Option<usize>, x: &[usize]) -> Option<Point> {
-        Some(Point(i?, *x.get(i?)?))
-    }
-}
-
-impl std::cmp::PartialOrd for Point {
-    fn partial_cmp(&self, other: &Point) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl std::cmp::Ord for Point {
-    fn cmp(&self, other: &Point) -> std::cmp::Ordering {
-        self.1.cmp(&other.1).then_with(|| self.0.cmp(&other.0))
-    }
-}
-
 use matrix::UTTable;
 
 /// Fully tabulating the answer to all queries with
@@ -105,111 +86,14 @@ impl TabulatedQuery {
             for j in i + 2..x.len() + 1 {
                 // Dynamic programming:
                 // Min val in [i,j) is either min in [i,j-1) or [j-1,j)
-                let left = Point::new(tbl[(i, j - 1)], x);
-                let current = Point::new(j - 1, x);
-                tbl[(i, j)] = std::cmp::min(left, current).0
+                tbl[(i, j)] = if x[tbl[(i, j - 1)]] <= x[j - 1] {
+                    tbl[(i, j - 1)]
+                } else {
+                    j - 1
+                };
             }
         }
         TabulatedQuery { tbl }
-    }
-}
-
-/// Storing values for all (i,i+2^k) indices and only those
-pub mod powers {
-    /// Type for powers of two, 2^k. Contains k, but wrapped in
-    /// a type so we don't confuse log-space with linear space.
-    #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-    pub struct Pow(pub usize);
-
-    impl Pow {
-        /// for a power Pow(k) get 2^k.
-        #[inline]
-        pub fn value(&self) -> usize {
-            1 << self.0
-        }
-    }
-
-    /// Get k such that 2**k is j rounded down to the
-    /// nearest power of 2.
-    /// j=1=2^0 => 0
-    /// j=2=2^1 => 1
-    /// j=3=2^1+1 => 1
-    /// j=4=2^2 => 2
-    /// and so on.
-    pub fn log2_down(j: usize) -> Pow {
-        assert!(j != 0); // not defined for zero
-
-        // Rounded down means finding the index of the first
-        // 1 in the bit-pattern. If j = 00010101110
-        // then 00010000000 (only first bit) is the closest
-        // power of two, and we want the position of that bit.
-        // j.leading_zeros() counts the number of leading zeros
-        // and we get the index by subtracting this
-        // from the total number of bits minus one.
-        Pow((usize::BITS - j.leading_zeros() - 1) as usize)
-        // usize::BITS and j.leading_zeros() will be u32, so
-        // we cast the result back to usize.
-    }
-
-    pub fn power_of_two(x: usize) -> bool {
-        (x == 0) || ((x & (x - 1)) == 0)
-    }
-
-    /// For n, get (rounded up) log2(n).
-    pub fn log2_up(n: usize) -> Pow {
-        // log_table_size(n) with n=2^k+m will always give us 2^{k+1},
-        // whether m is zero or not. We want 2^{k+1} when m > 0 and 2^k
-        // when m is zero, i.e. when n is a power of two.
-        // So we should subtract one from the exponent if n is a power of two.
-        let Pow(k) = log_table_size(n);
-        Pow(k - power_of_two(n) as usize)
-    }
-
-    /// We always have to add one to the exponent, because in log-space
-    /// we are working with 1-indexed (0-indexed in log-space) values,
-    /// so to have a table that can handle maximum value k, we need k+1
-    /// entires. That is what this function gives us.
-    pub fn log_table_size(n: usize) -> Pow {
-        let Pow(k) = log2_down(n);
-        Pow(k + 1)
-    }
-
-    /// From range [i,j), get values (k,j-2^k) where k is the offset
-    /// into the TwoD table to look up the value for [i,i+2^k) and [j-2^k,j)
-    /// from which we can get the RMQ.
-    pub fn power_index(i: usize, j: usize) -> ((usize, Pow), (usize, Pow)) {
-        let powk = log2_down(j - i);
-        ((i, powk), (j - powk.value(), powk))
-    }
-
-    /// A rather simple 2D array made from vectors of vectors.
-    /// There are better solutions, but I can implement those later
-    /// with the same interface.
-    pub struct Powers {
-        table: Vec<Vec<usize>>,
-    }
-
-    impl Powers {
-        pub fn new(n: usize) -> Powers {
-            let Pow(logn) = log_table_size(n);
-            let table = vec![vec![0; logn]; n];
-            Powers { table }
-        }
-    }
-
-    impl std::ops::Index<(usize, Pow)> for Powers {
-        type Output = usize;
-        fn index(&self, index: (usize, Pow)) -> &Self::Output {
-            let (i, Pow(k)) = index;
-            &self.table[i][k]
-        }
-    }
-
-    impl std::ops::IndexMut<(usize, Pow)> for Powers {
-        fn index_mut(&mut self, index: (usize, Pow)) -> &mut Self::Output {
-            let (i, Pow(k)) = index;
-            &mut self.table[i][k]
-        }
     }
 }
 
@@ -220,8 +104,7 @@ pub struct BlockSize(pub usize);
 pub struct BlockIdx(pub usize);
 
 pub fn block_size(n: usize) -> BlockSize {
-    let powers::Pow(block_size) = powers::log2_up(n);
-    BlockSize(block_size)
+    BlockSize(flog2(n) + 1)
 }
 
 pub fn round_down(i: usize, bs: BlockSize) -> (BlockIdx, usize) {
@@ -237,24 +120,22 @@ pub fn round_up(i: usize, bs: BlockSize) -> (BlockIdx, usize) {
 
 /// Reduce an array x to the smallest value in each block (of size block_size)
 /// and the index in the original array that this minimal value sits at.
-pub fn reduce_array(x: &[usize], block_size: BlockSize) -> (Vec<usize>, Vec<usize>) {
+pub fn reduce_array(x: &[usize], block_size: BlockSize) -> Vec<usize> {
     let BlockSize(bs) = block_size;
-    let mut indices: Vec<usize> = Vec::new();
     let mut values: Vec<usize> = Vec::new();
     let no_blocks = x.len() / bs;
     for block in 0..no_blocks {
         let begin = block * bs;
         let end = begin + bs;
+        // dbg!(begin, end);
         // naive rmq
         let y = &x[begin..end];
         let min_val = y.iter().min().unwrap();
         let pos = begin + y.iter().position(|a| a == min_val).unwrap();
-        let Point(pos, val) = Point::new(pos, x);
-        indices.push(pos);
-        values.push(val);
+        values.push(x[pos]);
     }
 
-    (indices, values)
+    values
 }
 
 /// Build a table of Ballot numbers B_pq from p=q=0 to p=q=b.
@@ -302,8 +183,7 @@ fn block_type(block: &[usize], b: usize, stack: &mut [i64], ballot: &Matrix) -> 
     num
 }
 
-/// Compute the block types for all blocks in x and compute the tables for the
-/// blocks we observe.
+/// Compute the block types and the tables for the blocks we observe in x.
 fn tabulate_blocks(x: &[usize], b: usize) -> (Vec<usize>, Vec<Option<TabulatedQuery>>) {
     // We need to round up to get the number of blocks here.
     // The reduced array handles blocks 0, 1, ..., x.len()/b but we
@@ -325,6 +205,7 @@ fn tabulate_blocks(x: &[usize], b: usize) -> (Vec<usize>, Vec<Option<TabulatedQu
         let block = &x[begin..end];
 
         let bt = block_type(block, b, &mut stack, &ballot);
+        dbg!(bt);
         block_types[i] = bt;
         if block_tables[bt].is_none() {
             block_tables[bt] = Some(TabulatedQuery::new(block));
@@ -350,7 +231,7 @@ impl<'a> Optimal<'a> {
         let b = std::cmp::max(4, b / 4); // I don't want too small blocks, so minimum is 4
         let block_size = BlockSize(b);
 
-        let (_, reduced_vals) = reduce_array(x, block_size);
+        let reduced_vals = reduce_array(x, block_size);
         let (block_types, block_tables) = tabulate_blocks(x, b);
 
         Optimal {
@@ -371,9 +252,9 @@ impl<'a> Optimal<'a> {
         let block_index = i / bs; // The index in the list of blocks
         let block_begin = block_index * bs; // The index the block starts at in x
 
-        // Get the table for this block by looking up the block type and then the
-        // table from the block type.
+        // Get the table for this block by looking up the block type...
         let btype = self.block_types[block_index];
+        // ... and then the table from the block type.
         let tbl = self.block_tables[btype].as_ref().unwrap();
 
         // Get RMQ and adjust the index back up, so it is relative to the start of the block.
